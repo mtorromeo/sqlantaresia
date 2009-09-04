7e61ac7e22f81eb0997ab11494121ffc7094a1ca
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2009-09-04 21:40:57 +0000

    Added py2exe setup script
    git-svn-id: https://sqlantaresia.googlecode.com/svn/trunk@26 a84d0858-22a7-11de-8b5c-13accb87f508

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import py2exe
+
+setup(name="sqlantaresia",
+	version="0.1",
+	author="Massimiliano Torromeo",
+	author_email="massimiliano.torromeo@gmail.com",
+	url="http://code.google.com/p/sqlantaresia/",
+	license="MIT License",
+	windows=[{"script": "SQLAntaresia.py"}],
+	options={"py2exe": {"skip_archive": True, "includes": ["sip", "PyQt4.QtSql"]}})
