e4444fb35fcf2bc518673d29b6d51b290d2fb7b6
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2009-11-28 20:51:06 +0000

    Added py2app support for mac osx
    git-svn-id: https://sqlantaresia.googlecode.com/svn/trunk@49 a84d0858-22a7-11de-8b5c-13accb87f508

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,49 @@
-from distutils.core import setup
-import py2exe
+"""
+py2app/py2exe build script for MyApplication.
 
-setup(name="sqlantaresia",
+Will automatically ensure that all build prerequisites are available
+via ez_setup
+
+Usage (Mac OS X):
+    python setup.py py2app
+
+Usage (Windows):
+    python setup.py py2exe
+"""
+import sys
+from setuptools import setup
+
+mainscript = 'SQLAntaresia.py'
+
+if sys.platform == 'darwin':
+	extra_options = dict(
+		setup_requires=['py2app'],
+		app=[mainscript],
+		options={"py2app": {
+			"argv_emulation": True,
+			"includes": ["PyQt4._qt"]
+		}},
+	)
+elif sys.platform == 'win32':
+	extra_options = dict(
+		setup_requires=['py2exe'],
+		windows=[{"script": mainscript}],
+		options={"py2exe": {
+			"skip_archive": True,
+			"includes": ["sip"]
+		}}
+	)
+else:
+	extra_options = dict(
+		scripts=[mainscript],
+	)
+
+setup(
+	name="sqlantaresia",
 	version="0.1",
 	author="Massimiliano Torromeo",
 	author_email="massimiliano.torromeo@gmail.com",
 	url="http://code.google.com/p/sqlantaresia/",
 	license="MIT License",
-	windows=[{"script": "SQLAntaresia.py"}],
-	options={"py2exe": {
-		"skip_archive": True,
-		"includes": ["sip"]
-	}}
+	**extra_options
 )
