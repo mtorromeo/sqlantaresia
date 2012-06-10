3a1aa4b2c638a79be0e784bc55ef2c82095d692c
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2012-06-10 17:37:19 +0200

    Edit cells by pressing the enter key

---
+++ sqlantaresia/EditableTableView.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QTableView, QAbstractItemView
+
+class EditableTableView(QTableView):
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return and self.state() != QAbstractItemView.EditingState:
+            selected = self.selectionModel().selectedIndexes()
+            if selected:
+                selected = selected[0]
+                self.setCurrentIndex(selected)
+                self.edit(selected)
+        else:
+            QTableView.keyPressEvent(self, event)
